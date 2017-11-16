39d9556bf3c02015164a37e25a63ec2db51c5996
liva <sap.pcmail@gmail.com>
2017-11-16 11:01:27 +0900

    bug fix

--- uio.sh
+++ uio.sh
@@ -1,17 +1,22 @@
-#!/bin/bash -x
+#!/bin/bash
+
+mkdir build-linux
+cd build-linux
+
+if [ $? -gt 0 ]; then
+    echo !!! Error: "build-linux" exists on the current directory. Please remove/rename it and retry this script. !!!
+    exit
+fi
 
 sudo su -c "grep '^deb ' /etc/apt/sources.list | sed 's/^deb/deb-src/g' > /etc/apt/sources.list.d/deb-src.list"
 sudo sed -i'~' -E "s@http://(..\.)?(archive|security)\.ubuntu\.com/ubuntu@http://linux.yz.yamagata-u.ac.jp/pub/linux/ubuntu-archive/@g" /etc/apt/sources.list
-cd $HOME
 
-sudo apt update -y
-sudo apt install -y git g++ make autoconf emacs language-pack-ja-base language-pack-ja gdb silversearcher-ag
+sudo apt update -qq
 
-sudo apt install -y build-essential kernel-package libssl-dev linux-headers-$(uname -r)
-apt source -y linux-source-4.4.0
-# sudo apt build-dep linux-source-$(uname -r)
-cd linux-4.4.0/
-make oldconfig
+sudo apt-get build-dep -qq linux-image-$(uname -r)
+apt-get source -y -qq linux-image-$(uname -r)
+cd linux-*
+yes "" | make oldconfig
 make prepare
 make scripts
 cp -v /usr/src/linux-headers-$(uname -r)/Module.symvers .
@@ -21,4 +26,4 @@ sudo make -C /lib/modules/$(uname -r)/build M=$(pwd) modules_install
 sudo depmod
 sudo modprobe uio
 sudo modprobe uio_pci_generic
-cd ../../../
+cd ../../../../
